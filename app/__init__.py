d7d4ff6c33f7dbbb1be0962c8d0e5c809dd32c11
MuhammadAman02 <mohammada@hst.ie>
2025-07-02 18:04:25 +0500

    Codegen: app/__init__.py

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+"""Apple Online Store Application Package"""
+
+__version__ = "1.0.0"
+__author__ = "HST AI"
+__description__ = "Apple Online Store - E-commerce Platform"
+
+__all__ = ["__version__", "__author__", "__description__"]
